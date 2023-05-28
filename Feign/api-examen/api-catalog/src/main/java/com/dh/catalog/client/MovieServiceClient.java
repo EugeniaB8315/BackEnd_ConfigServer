20337bd47e5660801fefb9c8651f5bd4d5598843
EugeniaB8315 <eugebonillo@gmail.com>
2023-05-28 20:02:24 -0300

    cloud, feign, gateway

---
+++ Feign/api-examen/api-catalog/src/main/java/com/dh/catalog/client/MovieServiceClient.java
@@ -0,0 +1,36 @@
+package com.dh.catalog.client;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+//@FeignClient(name="api-movie",url = "http://localhost:8085")
+@FeignClient(name="movie-service")
+public interface MovieServiceClient {
+
+	@GetMapping("/api/v1/movies/{genre}")
+	List<MovieDto> getMovieByGenre(@PathVariable (value = "genre") String genre);
+
+	@PostMapping("/save")
+	MovieDto saveMovie(@RequestBody MovieDto movie);
+
+	@Getter
+	@Setter
+	class MovieDto{
+		private Long id;
+
+		private String name;
+
+		private String genre;
+
+		private String urlStream;
+	}
+
+}
---
+++ gateway/api-examen/api-catalog/src/main/java/com/dh/catalog/client/MovieServiceClient.java
@@ -0,0 +1,33 @@
+package com.dh.catalog.client;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+//@FeignClient(name="api-movie",url = "http://localhost:8085")
+@FeignClient(name="movie-service")
+public interface MovieServiceClient {
+
+	@GetMapping("/api/v1/movies/{genre}")
+	List<MovieDto> getMovieByGenre(@PathVariable (value = "genre") String genre);
+
+	@Getter
+	@Setter
+	class MovieDto{
+		private Long id;
+
+		private String name;
+
+		private String genre;
+
+		private String urlStream;
+	}
+
+}
